0606791f4541df937288b050ffc5b78500788b7e
Roby <roberto.portoni@gmail.com>
2023-04-02 16:17:49 +0200

    esempio di test con rest-assured

--- src/test/java/org/acme/ApplicationSpecificationResourceTest.java
+++ src/test/java/org/acme/ApplicationSpecificationResourceTest.java
@@ -15,7 +15,17 @@ public class ApplicationSpecificationResourceTest {
           .when().get("/hello-resteasy")
           .then()
              .statusCode(200)
-             .body(is("Hello RESTEasy"));
+//             .body(is("{\"content\":\"<HEAD></HEAD><BODY></BODY>\",\"id\":2,\"ownerId\":\"1\",\"template-name\":\"Roby\",\"templateType\":\"IT\"}"));
+                    .body("content",is("<HEAD></HEAD><BODY></BODY>"))
+                .and()
+                    .body( "id", is(2))
+                .and()
+                    .body("ownerId", is("1"))
+                .and()
+                     .body("template-name", is("Roby"))
+                .and()
+                     .body("templateType", is("IT"));
     }
+
 
 }
